15a6bbb7823ff3b03afbbd1e81bd1d2fef6e9f44
Guillermo Villalobos <gmovida@gmail.com>
2017-10-03 22:55:22 -0600

    varios proyectos para prubar

---
+++ Clinica/Models/ClinicaContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Clinica.Models
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class ClinicaContext: DbContext
+    {
+        
+        public ClinicaContext()
+          : base("Clinica")
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public ClinicaContext(DbConnection existingConnection, bool contextOwnsConnection)
+          : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().MapToStoredProcedures();
+        }
+       
+    }
+}
---
+++ Clinica/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string email { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string password { get; set; }
+
+        public bool enabled { get; set; }
+        
+    }
+ 
+}
